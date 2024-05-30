e5b42b954a772bc91325420ae214ed77d80c8d1f
thegame4craft <48205247+thegame4craft@users.noreply.github.com>
2024-05-30 11:17:00 +0200

    Update deps.sh for arch linux

--- deps.sh
+++ deps.sh
@@ -7,5 +7,7 @@ if [[ -f "/etc/os-release" ]]; then
         sudo apt install wireguard-tools libgtk-3-dev libayatana-appindicator3-dev golang-go resolvconf
     elif [[ "${ID}" == "debian" ]]; then
         sudo apt install wireguard-tools libgtk-3-dev libayatana-appindicator3-dev golang-go resolvconf
+    elif [[ "${ID}" == "arch" ]]; then
+        sudo pacman -S wireguard-tools gtk3 libayatana-appindicator go openresolv
     fi
 fi
